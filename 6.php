5820f4f251093694c1918555052811dbb8bfe1f3
argajienbi <armin.gandi@gmail.com>
2018-12-01 12:19:16 +0700

    Add files via upload

---
+++ 6.php
@@ -0,0 +1,18 @@
+<?php
+
+///Jangan hapus tanpa kutip nya(")
+
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+
+///Ganti xxxxxxx dengan data punya kalian
+
+///Capture dulu apk nya ambil data yang di butuhkan
+
+$ticket = 
+"xxxxxxxxxcxxxxxxxxx";
+
+$meid = "xxxxxxxxxxxxxxxxxxxxx";
+
+$os="android";
+
+$sign="xxxxxxxxxxxxxxx";
